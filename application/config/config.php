1b639822747a7931aea7ec5b538774464f4d5bd2
PREMAWAISNAWA <premawaisnawa@gmail.com>
2018-08-21 18:25:41 +0800

    sip

--- application/config/config.php
+++ application/config/config.php
@@ -25,7 +25,7 @@ date_default_timezone_set('Asia/Makassar');
 */
 //$config['base_url'] = 'http://localhost/marketplace/'
 //$config['base_url'] = 'http://dinilaku.com';;
- $config['base_url'] = 'http://localhost/peken/';
+ $config['base_url'] = 'http://localhost/here_sell_well/';
 //  $root  = "http://".$_SERVER['HTTP_HOST'];
 // $root .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
 // $config['base_url']    = $root;
